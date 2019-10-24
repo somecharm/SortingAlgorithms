d1cf2c1487f232a74ff870043f6c7e802f82db92
somecharm <somecharm@gmail.com>
2019-10-24 14:28:42 +0300

    ksort

---
+++ src/ksort.java
@@ -0,0 +1,27 @@
+public class ksort {
+    public String[] items;
+
+    public ksort() {
+        items = new String[8 * 10 * 10];
+    }
+
+    public int index(String s) {
+        char A = s.charAt(0);
+        char M = s.charAt(2);
+        char N = s.charAt(1);
+
+        if (A >= 97 && A <= 104 && M >= 48 && M <= 57 && N >= 48 && N <= 57) {
+            return (A - 97) * 100 + (M - 48) * 10 + (N - 48);
+        }
+        return -1;
+    }
+
+    public boolean add(String s) {
+        int index = index(s);
+        if (index >= 0) {
+            items[index] = s;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/ksortTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ksortTest {
+
+    @Test
+    void index() {
+        ksort k = new ksort();
+        assertEquals(11,k.index("a11"));
+        assertEquals(111,k.index("b11"));
+        assertEquals(799,k.index("h99"));
+        assertEquals(-1,k.index("i99"));
+    }
+
+    @Test
+    void add() {
+        ksort k = new ksort();
+        assertTrue(k.add("a00"));
+        assertTrue(k.add("a10"));
+        assertTrue(k.add("a20"));
+        assertFalse(k.add("0jk"));
+        assertFalse(k.add("v15"));
+        System.out.println(Arrays.toString(k.items));
+    }
+}
